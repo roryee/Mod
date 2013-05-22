e73b0305f80fee68e0f395acb78d738fa84fc2b6
roryee <james.wyeth@gmail.com>
2013-05-21 20:27:26 -0700

    First main commit. Added basic barebones classes.

---
+++ mod_common/roryee/mod/lib/Reference.java
@@ -0,0 +1,9 @@
+package roryee.mod.lib;
+
+public class Reference {
+
+    public static final String MOD_ID = "roryee.mod";
+    public static final String MOD_NAME = "Mod!!!";
+    public static final String VERSION = "0.0.1";
+
+}
